49633e41d7081fc76b20043b0341a822e99e1f68
Shravan Asati <dev.shravan@protonmail.com>
2021-05-31 23:22:56 +0530

    🔥 minimal go server and home page

--- src/main.go
+++ src/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func loadPage(page string) ([]byte, error) {
+	content, e := ioutil.ReadFile("./templates/" + page)
+	if e != nil {
+		return nil, e
+	}
+	return content, nil
+}
+
+func main() {
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		c, e := loadPage("index.html")
+		if e != nil {
+			panic(e)
+		}
+		w.Write(c)
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
