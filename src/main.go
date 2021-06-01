5f469c2208f9eb1ae35eb47bf17e4d7a73e86b70
Shravan Asati <dev.shravan@protonmail.com>
2021-06-01 12:08:52 +0530

    🚑 removed occurences of panic

--- src/main.go
+++ src/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func loadPage(page string) ([]byte, error) {
 	content, e := ioutil.ReadFile("./templates/" + page)
 	if e != nil {
-		return nil, e
+		return []byte(strconv.Itoa(http.StatusInternalServerError)), e
 	}
 	return content, nil
 }
@@ -20,12 +21,12 @@ func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		c, e := loadPage("index.html")
 		if e != nil {
-			panic(e)
+			fmt.Println(e)
 		}
 		w.Write(c)
 	})
 	
 	port := os.Getenv("PORT")
 	if len(port) == 0 { port = "8080" }
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	fmt.Println(http.ListenAndServe(":"+port, nil))
 }
